27eb88660ab2b356a339dc778265a91b38a96b6e
Dan <thunderusnasdermn@yandex.ru>
2022-12-01 22:28:58 +0300

    fix: исправить ошибки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 const popup = document.querySelector('.popup');
-const popupName = popup.querySelector('.popup__name');
-const popupDescription = popup.querySelector('.popup__description');
+const popupForm = popup.querySelector('.popup__form');
+const popupName = popup.querySelector('.popup__input_name');
+const popupDescription = popup.querySelector('.popup__input_description');
 const popupCross = popup.querySelector('.popup__cross');
-const popupButton = popup.querySelector('.popup__button');
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
 const profileName = profile.querySelector('.profile__name');
 const profileDescription = profile.querySelector('.profile__description');
-popupName.value=profileName.textContent;
-popupDescription.value=profileDescription.textContent;
 
 function popupOpen() {
   popup.classList.add('popup_opened');
+  popupName.value=profileName.textContent;
+  popupDescription.value=profileDescription.textContent;
 }
 
 function popupClose() {
@@ -22,9 +22,9 @@ function popupSave() {
   event.preventDefault();
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
-  popup.classList.remove('popup_opened');
+  popupClose();
 }
 
 editButton.addEventListener('click', popupOpen);
 popupCross.addEventListener('click', popupClose);
-popupButton.addEventListener('click', popupSave);
+popupForm.addEventListener('submit', popupSave);
